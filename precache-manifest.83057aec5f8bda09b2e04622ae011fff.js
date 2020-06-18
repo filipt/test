f0ebe9b62ecd05b0e94dde8637ad63180330c5f5
filipt <filip@pointapi.com>
2020-06-17 20:01:11 -0700

    Updates

---
+++ precache-manifest.83057aec5f8bda09b2e04622ae011fff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a03215f76bfd3a52b872ef0802d5422",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "9df81e84e9ccd5ddc2e9",
+    "url": "/test/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b2a45dc4cd79468e9ea3",
+    "url": "/test/static/js/2.8f59fa54.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test/static/js/2.8f59fa54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9df81e84e9ccd5ddc2e9",
+    "url": "/test/static/js/main.a15905d9.chunk.js"
+  },
+  {
+    "revision": "29d9a3cf2ee130fa9d36",
+    "url": "/test/static/js/runtime-main.2c1f07ac.js"
+  }
+]);
